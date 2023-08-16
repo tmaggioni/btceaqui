2e69c2fde7a59207ddc5f1035908863501d1bf1f
Maggioni <t_maggioni@hotmail.com>
2023-08-16 01:56:50 -0300

    ajuste slice

--- src/pages/slice-simulator.tsx
+++ src/pages/slice-simulator.tsx
@@ -3,12 +3,8 @@
 import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../slices";
+// import { components } from "../slices";
 
 export default function SliceSimulatorPage() {
-  return (
-    <SliceSimulator
-      sliceZone={(props) => <SliceZone {...props} components={components} />}
-    />
-  );
+  return <SliceSimulator sliceZone={(props) => <SliceZone {...props} />} />;
 }
